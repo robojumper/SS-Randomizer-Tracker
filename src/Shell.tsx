c384f8adcc19303b03698fb267e1a3226807aa56
robojumper <robojumper@gmail.com>
2023-12-25 20:41:10 +0100

    Better error handling

--- src/Shell.tsx
+++ src/Shell.tsx
@@ -1,39 +1,52 @@
 import { useCallback, useEffect, useState } from 'react';
 import TrackerContainer from './Tracker';
 import { useDispatch, useSelector } from 'react-redux';
-import { loadLogic } from './logic/slice';
+import { loadLogic, setLoadingError } from './logic/slice';
 import { rawLogicSelector, rawOptionsSelector } from './logic/selectors';
 import { acceptSettings } from './tracker/slice';
 import { defaultSettings } from './permalink/Settings';
-import { RemoteReference, defaultUpstream, formatRemote, loadRemoteLogic, promptRemote } from './loader/LogicLoader';
+import { RemoteReference, defaultUpstream, formatRemote, loadRemoteLogic } from './loader/LogicLoader';
+import { RootState } from './store/store';
+import ErrorPage from './ErrorPage';
 
 export default function Shell() {
     const dispatch = useDispatch();
     const [loadingRemote, setLoadingRemote] = useState<RemoteReference | undefined>();
 
-    const load = useCallback(async () => {
-        const storedRemote = localStorage.getItem('ssrTrackerRemoteLogic');
-        const remote = storedRemote !== null ? JSON.parse(storedRemote) as RemoteReference : defaultUpstream;
+    const load = useCallback(async (remote: RemoteReference) => {
         setLoadingRemote(remote);
-        const [logic, options] = await loadRemoteLogic(remote);
-        dispatch(acceptSettings({ settings: defaultSettings(options), initialLoad: true }))
-        dispatch(loadLogic({ logic, options, remote }));
-        setLoadingRemote(undefined);
+        try {
+            const [logic, options] = await loadRemoteLogic(remote);
+            dispatch(acceptSettings({ settings: defaultSettings(options), initialLoad: true }))
+            dispatch(loadLogic({ logic, options, remote }));
+        } catch (e) {
+            dispatch(setLoadingError({ error: e ?? 'Unknown error' }));
+        } finally {
+            setLoadingRemote(undefined);
+        }
     }, [dispatch]);
 
     useEffect(() => {
-        load();
+        const storedRemote = localStorage.getItem('ssrTrackerRemoteLogic');
+        const remote = storedRemote !== null ? JSON.parse(storedRemote) as RemoteReference : defaultUpstream;
+        load(remote);
     }, [load]);
 
     const logic = useSelector(rawLogicSelector);
     const options = useSelector(rawOptionsSelector);
+    const loadingError = useSelector((state: RootState) => state.logic.error);
 
     if (!logic || !options) {
-        return <>
-            Loading {loadingRemote ? formatRemote(loadingRemote) : ''}...
-            <br />
-            <button onClick={() => promptRemote(dispatch, loadingRemote, true)}>Load a different version</button>
-        </>;
+        return (
+            <>
+                {loadingError ? <ErrorPage error={loadingError} resetErrorBoundary={() => undefined} /> : (
+                    <>
+                        Loading{' '}
+                        {loadingRemote ? formatRemote(loadingRemote) : ''}...
+                    </>
+                )}
+            </>
+        );
     }
 
     return (
--- src/loader/LogicLoader.ts
+++ src/loader/LogicLoader.ts
@@ -2,7 +2,7 @@ import { load } from 'js-yaml';
 import { RawLogic } from '../logic/UpstreamTypes';
 import { OptionDefs } from '../permalink/SettingsTypes';
 import { AppDispatch } from '../store/store';
-import { loadLogic } from '../logic/slice';
+import { loadLogic, setLoadingError } from '../logic/slice';
 import { acceptSettings } from '../tracker/slice';
 import { defaultSettings } from '../permalink/Settings';
 
@@ -85,7 +85,7 @@ export async function loadRemoteLogic(remote: RemoteReference): Promise<[RawLogi
     return [logic, options];
 }
 
-export function promptRemote(dispatch: AppDispatch, currentRemote: RemoteReference | undefined, firstTime = false) {
+export async function promptRemote(dispatch: AppDispatch, currentRemote: RemoteReference | undefined, firstTime = false) {
     const val = prompt('Enter Randomizer Version or branch', currentRemote?.type === 'releaseVersion' ? currentRemote.versionTag : "");
     if (!val) {
         return;
@@ -95,13 +95,15 @@ export function promptRemote(dispatch: AppDispatch, currentRemote: RemoteReferen
         alert('Invalid Randomizer Version (try v.2.1.1)');
     }
 
-    (async () => {
-        try {
-            const [logic, options] = await loadRemoteLogic(remote!);
-            dispatch(acceptSettings({ settings: defaultSettings(options), initialLoad: firstTime }))
-            dispatch(loadLogic({ logic, options, remote: remote! }));
-        } catch (e) {
-            alert('Failed to load: ' + (e && typeof e === 'object' && 'message' in e ? e.message as string : JSON.stringify(e)));
-        }
-    })();
+    try {
+        await loadRemote(dispatch, remote!, firstTime);
+    } catch (e) {
+        dispatch(setLoadingError({ error: e ?? 'Unknown error' }));
+    }
+}
+
+export async function loadRemote(dispatch: AppDispatch, remote: RemoteReference, firstTime = false) {
+    const [logic, options] = await loadRemoteLogic(remote);
+    dispatch(acceptSettings({ settings: defaultSettings(options), initialLoad: firstTime }))
+    dispatch(loadLogic({ logic, options, remote: remote }));
 }
