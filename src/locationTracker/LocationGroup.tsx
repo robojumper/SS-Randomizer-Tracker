afc024f0a2c125c9a2a761608c5ef72ae50f0842
robojumper <robojumper@gmail.com>
2024-01-10 00:19:43 +0100

    Location layout

--- src/locationTracker/LocationGroup.tsx
+++ src/locationTracker/LocationGroup.tsx
@@ -1,21 +1,38 @@
+import { useSelector } from 'react-redux';
 import Location from './Location';
 import { Col, Row } from 'react-bootstrap';
+import { locationLayoutSelector } from '../customization/selectors';
+import _ from 'lodash';
 
 export default function LocationGroup({
     locations,
 }: {
     /* the list of locations this group contains */
-    locations: string[],
+    locations: string[];
 }) {
+    const mapMode = useSelector(locationLayoutSelector) === 'map';
+    const numColumns = mapMode ? 2 : 1;
     const locationRows = locations.map((location) => (
-        <Row key={location} style={{ paddingTop: '2%', paddingBottom: '2%', border: `1px solid var(--scheme-text)` }}>
-            <Location id={location}
-            />
+        <Row
+            key={location}
+            style={{
+                paddingTop: '2%',
+                paddingBottom: '2%',
+                border: `1px solid var(--scheme-text)`,
+            }}
+        >
+            <Location id={location} />
         </Row>
     ));
+    const locationColumns = _.chunk(
+        locationRows,
+        Math.ceil(_.size(locationRows) / numColumns),
+    );
     return (
-        <Col>
-            {locationRows}
-        </Col>
+        <>
+            {locationColumns.map((rows, index) => (
+                <Col key={index}>{rows}</Col>
+            ))}
+        </>
     );
 }
