5844c9a578c7070f5a2bedf5f50d94de6704b37a
robojumper <robojumper@gmail.com>
2024-10-24 14:26:00 +0200

    Check config js files too

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
-    },
-    plugins: [react()],
-    test: {
-        globals: true,
-        environment: 'jsdom'
-    }
-  };
+    return {
+        build: {
+            outDir: 'build',
+        },
+        plugins: [react()],
+        test: {
+            globals: true,
+            environment: 'jsdom',
+        },
+    };
 });
